aa3b5ffda58a8940f81c8e83289b936532021a19
zjt <zjt@example.com>
2023-03-13 17:58:32 +0800

    add

---
+++ project00/src/test/java/com/zjt/project00/Project00ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zjt.project00;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project00ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
