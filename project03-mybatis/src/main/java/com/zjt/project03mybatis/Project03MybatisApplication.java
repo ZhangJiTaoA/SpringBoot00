f337a29a140b392bb44609bec45fc16826ad7358
zjt <zjt@example.com>
2023-03-14 10:49:36 +0800

    add

---
+++ project03-mybatis/src/main/java/com/zjt/project03mybatis/Project03MybatisApplication.java
@@ -0,0 +1,13 @@
+package com.zjt.project03mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project03MybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Project03MybatisApplication.class, args);
+    }
+
+}
